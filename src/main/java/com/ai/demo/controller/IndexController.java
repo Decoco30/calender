4a83bb33f2132183f6a692ec92a3ac73e1d42b23
Decoco30 <marimoex713@gmail.com>
2020-03-08 00:40:48 +0900

    カレンダー_曜日制御追加

--- src/main/java/com/ai/demo/controller/IndexController.java
+++ src/main/java/com/ai/demo/controller/IndexController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.spi.CalendarDataProvider;
 
 @Controller
 public class IndexController {
@@ -21,11 +22,18 @@ public class IndexController {
 
     @GetMapping("/calender")
     public String hello(Model model) {
+        Calendar calendar = Calendar.getInstance();
+calendar.set(2020,Calendar.APRIL,01);
+int dayOfWeek = calendar.get(calendar.DAY_OF_WEEK);
+System.out.println(dayOfWeek);
         List<List> month = new ArrayList<List>();
         List<Integer> week = new ArrayList<Integer>();
+        for(int j=1;j <= dayOfWeek-1;j++){
+            week.add(0);
+        }
         for (int i = 1; i <= 31; i++) {
             week.add(i);
-            if (i % 7 == 0) {
+            if (i % 7 == 0 + dayOfWeek) {
                 month.add(new ArrayList(week));
                 week.clear();
             }
